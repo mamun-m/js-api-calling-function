07d4eb3a8307dc517363e60887d41c9596b4f066
Mohammad mamun <miam6829@gmail.com>
2024-08-23 14:48:43 +0600

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-// const studentinfo = {
-//      name : "mohammad mamun",
-//      age : 21,
-//      gpa : 3.95,
-//     lang : {
-//      beginner :"bangla",
-//      avdance : "english"
-//   }
-// }
-// const {name,age,gpa,lang}=studentinfo;
-// console.log(`${name}:${age}:${gpa}:${lang}`)
-
-
-
-const studentinfo ={
-     name :"mia mamun khan",
-     age : 21,
-     id: 104,
+const firstfunction =(x)=>{
+  console.log(`number is ${x} : ${x*x} `)
 }
 
-const firstfunction =({name,age,id})=>{
-   console.log(`${name} :${age} ${id}`)
+// const y=firstfunction;
+// y(5);
+
+const secondfunction =(num, call)=>{
+  call(num)
 }
-firstfunction(studentinfo)
+secondfunction(6, firstfunction)
